05609bf391bc1d7d945d1f6cd53ded85ed53e6bb
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2025-05-25 15:10:58 -0400

    Update cruel.bat

--- cruel.bat
+++ cruel.bat
@@ -1,7 +1,14 @@
 @echo off
 
-start steam://rungameid/1388770
+start "" steam://rungameid/1388770
 
-timeout /t 2 /nobreak >nul
+start "" powershell -NoProfile -WindowStyle Hidden -Command ^
+"Add-Type -AssemblyName System.Windows.Forms; ^
+$n = New-Object System.Windows.Forms.NotifyIcon; ^
+$n.Icon = [System.Drawing.SystemIcons]::Information; ^
+$n.Visible = $true; ^
+$n.ShowBalloonTip(5000, 'Notification', 'get playing lil boy', [System.Windows.Forms.ToolTipIcon]::None); ^
+Start-Sleep -Seconds 6; ^
+$n.Dispose();"
 
-powershell -NoProfile -Command "Add-Type -AssemblyName System.Windows.Forms; $n = New-Object System.Windows.Forms.NotifyIcon; $n.Icon = [System.Drawing.SystemIcons]::Information; $n.Visible = $true; $n.ShowBalloonTip(5000, 'Notification', 'get playing lil boy', [System.Windows.Forms.ToolTipIcon]::None); Start-Sleep -Seconds 6; $n.Dispose();"
+exit
